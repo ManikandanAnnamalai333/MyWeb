068fbb31c7199dac6f13a679f60d3e31c7efdfdd
ManikandanAnnamalai333 <manikandan482003@gmail.com>
2025-07-24 09:55:25 +0530

    Add files via upload

---
+++ ApiConsumer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO; 
+using System.Text;
+class Program
+{
+    private static readonly HttpClient _httpClient=new HttpClient();
+    public static readonly string API_BASE_URL = " http://localhost:5023/games/2";
+    static async Task Main(string[] args)
+    {
+        HttpResponseMessage response=await _httpClient.GetAsync(API_BASE_URL);                  
+        response.EnsureSuccessStatusCode();
+        string responseBody=await response.Content.ReadAsStringAsync();
+        Console.WriteLine(responseBody);
+    }               
+}
